32182e34037e5a2037c13e92636bdd98cd492f16
Santiago Pulido <spulido@newo.co>
2023-11-29 11:54:43 -0500

    maquetacion de invitados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  useQR: any;
+  qrDisable: any;
+  active: boolean = false;
 
   constructor() {}
 
+  ngOnInit() {
+    if (this.active) {
+      this.useQR = this.generateQRCode('321654', 300);
+    } else {
+      this.useQR= this.generateQRCode('0', 300);
+    }
+    console.log(this.useQR)
+  }
+
+
+  generateQRCode(sede_id: string, size: number): string {
+    const canvas = document.createElement('canvas');
+      QRCode.toCanvas(canvas, sede_id, { width: size }, (error: any) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+    return canvas.toDataURL('image/png');
+  }
 }
